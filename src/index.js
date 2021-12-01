9165b1aa14bd2407d578c4081dca4f19dc1ee3e0
Avinandan <avinandan.agt@gmail.com>
2021-12-01 21:31:41 +0530

    bootstrap error

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
+import 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
 import 'font-awesome/css/font-awesome.min.css';
 
