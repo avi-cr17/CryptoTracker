be645ae342af3cbebd13e6fc0fc9914b0a774904
Avinandan <avinandan.agt@gmail.com>
2022-02-10 21:23:06 +0530

    final windows commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
 
 
-import center from '../public/images/img.png'
-import left from './collection/left.jpg'
-import right from './collection/right.jpg'
+import center from './collection/img4.png'
+import right from './collection/img.png'
+import left from './collection/ing5.png'
 import {useEffect, useRef, useState } from 'react';
 
-import {TweenMax,Power3} from 'gsap'
+import {TweenMax,Power4,Power3,gsap,Expo,Circ,Bounce} from 'gsap'
 import { TimelineLite } from 'gsap/gsap-core';
 import {useNavigate} from 'react-router-dom';
 
@@ -26,16 +26,22 @@ function App() {
 
   let tl = new TimelineLite();
 
+  var tll = gsap.timeline();
+
   useEffect(()=>{
     TweenMax.to(app,0,{css:{visibility:'visible'}})
     console.log('yes');
 
-    tl.from(leftimg,1.2,{x:12000,ease: Power3.easeInOut})
-      .from(leftimg,2,{scale:1.6, ease: Power3.easeOut},1)
-      .from(centerimg,1.2,{x:2200,ease: Power3.easeInOut},3.0)
-      .from(centerimg,2,{scale:1.6, ease: Power3.easeOut}, 1)
-      .from(rightimg,1.2,{x:3200,ease: Power3.easeInOut},4.0)
-      .from(rightimg,2,{scale:1.6, ease: Power3.easeOut}, 1)
+    
+
+  tll.from('body',{scale:1,duration:1,opacity:0,ease:Circ.easeIn,delay:0.2})
+  .from(leftimg,{x:1500,duration:2,ease:Power4.easeOut,delay:0.2})
+  .from(centerimg,{x:2000,duration:2,ease:Power4.easeOut,delay:0.2})
+  .from(rightimg,{x:1400,duration:3,ease:Circ.easeOut,delay:0.2})
+  .to('svg',{clipPath:'polygon(0 0, 100% 0, 100% 100%, 0 100%)', ease:Power4.easeOut,duration:2,delay:1})
+  .to('svg',{clipPath:'polygon(0 100%, 100% 100%, 100% 100%, 0 100%)',ease:Circ.easeOut,duration:2,delay:0.3})
+  .to('.heading',{opacity: 1,ease:Circ.easeOut,duration:2,delay:0.5})
+  
 
 
   },[])
@@ -50,7 +56,7 @@ function App() {
       
       <div className=" row g-0 no-gutters"  >
 
-      <img src={left} className={`img col-md-`+l +` col-sm-8`} 
+      <div  className={`img col-md-`+l +` col-sm-8`} 
        ref={el=> leftimg=el} 
       onMouseEnter={()=>{
         setL('4');
@@ -64,13 +70,16 @@ function App() {
 
       }}
       
-      />
-      <img src={center} className={`img col-md-`+c +` col-sm-8`}
+      >
+
+      </div>
+      <div  className={`img3 col-md-`+c +` col-sm-8`}
        ref={el=> centerimg=el}
       onMouseEnter={()=>{
         setL('2');
         setC('8');
         setR('2');
+        
       }}   
       onMouseLeave={()=>{
         setL('3');
@@ -79,9 +88,17 @@ function App() {
 
       }} 
       onClick={centerImageClick}
-      />
+      >
+        <svg className='svg' xmlns="http://www.w3.org/2000/svg" width="128" height="437" viewBox="0 0 128 437" fill="none">
+
+        <path d="M63 436.967L127.859 327.758L0.851345 326.193L63 436.967ZM75.2187 338.11L79.3701 1.13551L57.3718 0.864496L53.2204 337.839L75.2187 338.11Z" fill="#FF2222"/>
+        </svg>
+
+        <p className='heading'>Crypto Charts</p>
+        
+      </div>
       
-      <img src={right} className={`img2 col-md-`+r +` col-sm-8`}
+      <div className={`img2 col-md-`+r +` col-sm-8`}
        ref={el=> rightimg=el}
       onMouseEnter={()=>{
         setL('2');
@@ -94,7 +111,9 @@ function App() {
         setR('3');
 
       }}
-      />
+      >
+
+      </div>
 
 
 
